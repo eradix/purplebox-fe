070ebc3ab822cf8b6728bb42a6905e86c0ce394a
Charles Pitagan <charles.pitagan@norimaconsulting.com>
2022-11-29 12:16:06 +0800

    added add to cart feature

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,21 +1,41 @@
 import React from "react";
 import { useEffect } from "react";
+import { FaCheck } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
-import almond from "../assets/img/almond.jpg";
+import Modal from "../components/AlertModal";
+import { addToCart, orderActions } from "../store/order-slice";
 import { getProduct } from "../store/product-slice";
 
 const Product = () => {
   const { product_id } = useParams();
   const dispatch = useDispatch();
   const { product } = useSelector((state) => state.product);
+  const { form, success } = useSelector((state) => state.order);
 
   useEffect(() => {
     dispatch(getProduct(product_id));
+    const value = product_id;
+    const name = "product_id"
+    dispatch(orderActions.setForm({ name, value }));
   }, []);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    dispatch(orderActions.setForm({ name, value }));
+  };
 
   return (
     <>
+      {success && (
+        <Modal
+          icon={<FaCheck className="text-green-500 text-4xl" />}
+          status={"Success"}
+          message={"Added Cart Successful"}
+        />
+      )}
+
       <div className="md:h-screen flex items-center justify-center">
         <div className="md:flex items-center gap-10 my-12 md:my-0 md:mx-24  w-full justify-between ">
           <img
@@ -32,7 +52,10 @@ const Product = () => {
               <input
                 type="number"
                 placeholder="Quantity"
+                name="quantity"
+                value={form.quantity}
                 className="py-3 px-5 border w-full placeholder-black"
+                onChange={(e) => handleChange(e)}
               />
             </div>
             {product.type === "Cakes" && (
@@ -40,31 +63,22 @@ const Product = () => {
                 <input
                   type="text"
                   placeholder="What Message Would You Like On The Cake?"
+                  name="message"
+                  value={form.message}
+                  onChange={(e) => handleChange(e)}
                   className="py-3 px-5 border w-full placeholder-black"
                 />
               </div>
             )}
-            {/* <div className="flex">
-              <div className="">
-                <select
-                  className="py-3 px-5 border"
-                  aria-label="Default select example"
-                >
-                  <option defaultValue={"0"}>
-                    Will you be needing a candle with your Cake?
-                  </option>
-                  <option value="1">No</option>
-                  <option value="2">Yes - blue</option>
-                  <option value="3">Yes - pink</option>
-                </select>
-              </div>
-            </div> */}
 
             <div className="my-3">
               <p className="text-gray-500">{product.description}</p>
             </div>
             <div>
-              <button className="py-3 px-5 bg-indigo-500 text-white w-full ">
+              <button
+                onClick={(e) => dispatch(addToCart(form))}
+                className="py-3 px-5 bg-indigo-500 text-white w-full "
+              >
                 Add to Cart
               </button>
             </div>
--- src/store/order-slice.js
+++ src/store/order-slice.js
@@ -1,23 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const addToCart = createAsyncThunk(
+  "order/addtoCart",
+  async (payload, thunkAPI) => {
+    try {
+      const resp = await axios.post(
+        `${process.env.REACT_APP_API_URL}/api/orders`,
+        payload
+      );
+      return resp.data;
+    } catch (err) {
+      return err;
+    }
+  }
+);
 
 const orderSlice = createSlice({
   name: "order",
   initialState: {
     allOrders: [],
     form: {
-      custName: "",
-      prodImage: "",
-      prodName: "",
-      qty: "",
-      totalPrice: "",
-      status: "",
+      product_id: "",
+      quantity: "",
+      status: "On Cart",
+      message: "",
     },
     showModal: false,
     edit: true,
+    success: false,
   },
   reducers: {
     setForm(state, action) {
       state.form[action.payload.name] = action.payload.value;
+    },
+    resetForm(state) {
+      state.form = {
+        product_id: "",
+        quantity: "",
+        status: "On Cart",
+        message: "",
+      };
     },
     setShowModal(state, action) {
       state.showModal = action.payload;
@@ -30,6 +53,22 @@ const orderSlice = createSlice({
     },
     reset(state) {
       state.allUsers = [];
+    },
+    setSuccess(state, action) {
+      state.success = action.payload;
+    },
+  },
+  extraReducers: {
+    [addToCart.pending]: (state) => {
+      console.log("loading");
+    },
+    [addToCart.fulfilled]: (state, action) => {
+      console.log("fullfilled");
+      state.success = true;
+      state.success = false
+    },
+    [addToCart.rejected]: (state) => {
+      console.log("rejected");
     },
   },
 });
